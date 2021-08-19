d29a17ea9ef2cfc8d39fb0f99a895b1bd4008238
dev-sptg <developer@smartpelican.com>
2021-08-19 17:25:25 +0300

    Release 0.7.7

---
+++ README.md
@@ -0,0 +1,16 @@
+# LSP-leo
+
+Leo support for Sublime's LSP plugin provided through leo-language-server.
+Full-featured IDE with the compiler, package manager, and other helpful tools is here [Aleo Studio](https://aleo.studio/).
+
+### Installation
+
+- Install [LSP](https://packagecontrol.io/packages/LSP) and [LSP-leo](https://packagecontrol.io/packages/LSP-leo) from Package Control.
+- Restart Sublime.
+
+### Configuration
+
+There are some ways to configure the package and the language server.
+
+- From `Preferences > Package Settings > LSP > Servers > LSP-leo`
+- From the command palette `Preferences: LSP-leo Settings`
---
+++ plugin.py
@@ -0,0 +1,150 @@
+import os
+from threading import Timer
+import sublime, sublime_plugin
+from lsp_utils import NpmClientHandler
+from LSP.plugin.core.registry import windows
+from LSP.plugin.core.protocol import Request, Range
+from LSP.plugin.core.views import range_to_region, region_to_range
+from LSP.plugin.core.typing import Any, Dict
+from LSP.plugin.core.url import view_to_uri
+from lsp_utils import request_handler
+from LSP.plugin import uri_to_filename
+
+def plugin_loaded() -> None:
+    LspLeoPlugin.setup()
+    setup_leohover_settings()
+
+def plugin_unloaded() -> None:
+    LspLeoPlugin.cleanup()
+
+def setup_leohover_settings() -> None:  
+    preferences_filename = 'Preferences.sublime-settings'
+    preferences = sublime.load_settings(preferences_filename)
+    value = preferences.get("mdpopups.sublime_user_lang_map")
+    value["leohover"] = (('leohover',), ('LSP-leo/leoHover',))
+    preferences.set("mdpopups.sublime_user_lang_map", value)
+    sublime.save_settings(preferences_filename)
+
+class LspLeoPlugin(NpmClientHandler):
+    package_name = __package__
+    server_directory = 'language-server'
+    server_binary_path = os.path.join(server_directory, 'server.js')
+    
+    @request_handler('ColoringService.colorize')
+    def on_coloring_service_colorize(self, request, response):
+        filename = uri_to_filename(request['uri'])
+        view = sublime.active_window().find_open_file(filename)
+
+        if view:
+            syntax_coloring = SyntaxColoring()
+            syntax_coloring.view = view
+            syntax_coloring.colorize(request)
+        # Server doesn't require any specific response.
+        response(None)
+
+
+def setListener(view):
+    listener = windows.listener_for_view(view)
+    uri = view_to_uri(view)
+    if uri.endswith('.leo'):
+        view.assign_syntax("Packages/LSP-leo/leo.tmLanguage")
+        listener._language_id = "leo"
+    if uri.endswith('.in'):
+        view.assign_syntax("Packages/LSP-leo/leoInput.tmLanguage")
+        listener._language_id = "leoInput"
+    if uri.endswith('.state'):
+        view.assign_syntax("Packages/LSP-leo/leoInput.tmLanguage")
+        listener._language_id = "leoInput"
+    if uri.endswith('.out'):
+        view.assign_syntax("Packages/LSP-leo/leoInput.tmLanguage")
+        listener._language_id = "leoInput"
+    if uri.endswith('.toml'):
+        view.assign_syntax("Packages/LSP-leo/leoToml.tmLanguage")
+        listener._language_id = "leoToml"
+
+    if listener:
+        language = listener.get_language_id()
+        if language == "leo":    
+            exists = bool(listener.session_async('hoverProvider'))
+            if not exists:
+                # if there is no listener need to wait for it
+                print("No listener found!")
+                view.settings().set("lsp_uri", uri)
+                Timer(1.0, setListener, (view,)).start()
+
+def sendColorizeRequest(view):
+    listener = windows.listener_for_view(view)
+    if listener:
+        language = listener.get_language_id()
+        if language == "leo":    
+            exists = bool(listener.session_async('hoverProvider'))
+            if not exists:
+                # if there is no listener need to wait for it
+                print("No listener found!")
+                return
+            for session in listener.sessions_async():
+                uri = view_to_uri(view)
+                params = {"uri": uri}
+                request = Request("ColoringService.colorize", params)
+                session.send_request_async(request, lambda res: res, lambda res: res)
+
+
+class SyntaxColoringViewListener(sublime_plugin.ViewEventListener):
+    def on_activated_async(self):
+        setListener(self.view)       
+    def on_load_async(self):
+        setListener(self.view)
+    def on_modified_async(self):
+        setListener(self.view)
+    def on_selection_modified_async(self):
+        setListener(self.view)
+        sendColorizeRequest(self.view)
+
+class SyntaxColoring():
+    def colorize(self, request) -> None:
+        for key, values in request["scopes"].items():
+            if len(values):
+                flags = sublime.DRAW_NO_OUTLINE
+                regularScope = key
+                highlightedScope = key.replace("server.leo", "highlight.server.leo")
+                self.view.erase_regions(regularScope)
+                self.view.erase_regions(highlightedScope)
+                regularRegions = []
+                highlightedRegions = []
+                for item in values:
+                    lsp_range = self.server_range_to_lsp(item)
+                    sublime_range = Range.from_lsp(lsp_range)
+                    region = range_to_region(sublime_range, self.view)
+                    selected_row = self.view.sel()
+                    cursorRegion = selected_row[0]
+                    scope = regularScope
+                    is_point = cursorRegion.begin() == cursorRegion.end()
+                    
+                    if not is_point and selected_row.contains(region):
+                        scope = highlightedScope
+
+                    if is_point:
+                        cursorRange = region_to_range(self.view, cursorRegion)
+                        if sublime_range.start.row == cursorRange.start.row:
+                            scope = highlightedScope
+
+                    if scope == regularScope:
+                        regularRegions.append(region)
+                    else:
+                        highlightedRegions.append(region)
+
+                self.view.add_regions(regularScope, regularRegions, scope=regularScope, flags=flags)
+                self.view.add_regions(highlightedScope, highlightedRegions, scope=highlightedScope, flags=flags)
+                    
+
+    def server_range_to_lsp(self, sever_range: Dict[str, Any]) -> Dict[str, Any]:
+        return {
+            'start': {
+                "line": sever_range["start"]["row"],
+                "character": sever_range["start"]["column"]
+            },
+            'end': {
+                "line": sever_range["end"]["row"],
+                "character": sever_range["end"]["column"]
+            }
+        }
